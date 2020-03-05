dfb540e571b90217bf50f63190e3206e20aaca42
Maxim <zenit77798@yandex.ru>
2020-03-06 03:49:40 +0800

    Change builder configuration

--- builder.config.js
+++ builder.config.js
@@ -1,71 +1,72 @@
-const ICONS_DIR = 'build/icons/'
+/* eslint-disable no-template-curly-in-string */
+const ICONS_DIR = "build/icons/";
 
 const windowsOS = {
-  win: {
-    icon: ICONS_DIR + 'win-icon.ico',
-    publisherName: 'michal',
-    target: 'nsis'
-  },
+    win: {
+        icon: ICONS_DIR + "512x512.ico",
+        publisherName: "Maxim <purplehorrorrus@gmail.com>",
+        target: "nsis"
+    },
 
-  nsis: {
-    differentialPackage: true
-  }
-}
+    nsis: {
+        differentialPackage: true
+    }
+};
 
-const linuxOS = {
-  linux: {
-    icon: ICONS_DIR,
-    target: 'deb'
-  }
-}
+// const linuxOS = {
+//   linux: {
+//     icon: ICONS_DIR,
+//     target: 'deb'
+//   }
+// }
 
-const macOS = {
-  mac: {
-    target: 'dmg',
-    icon: ICONS_DIR + 'con.icns'
-  },
-  dmg: {
-    contents: [
-      {
-        x: 410,
-        y: 150,
-        type: 'link',
-        path: '/Applications'
-      },
-      {
-        x: 130,
-        y: 150,
-        type: 'file'
-      }
-    ]
-  }
-}
+// const macOS = {
+//   mac: {
+//     target: 'dmg',
+//     icon: ICONS_DIR + 'con.icns'
+//   },
+//   dmg: {
+//     contents: [
+//       {
+//         x: 410,
+//         y: 150,
+//         type: 'link',
+//         path: '/Applications'
+//       },
+//       {
+//         x: 130,
+//         y: 150,
+//         type: 'file'
+//       }
+//     ]
+//   }
+// }
 
 module.exports = {
-  asar: false,
-  productName: 'My browser',
-  appId: 'org.michalzarach.my-browser',
-  artifactName: 'my-browser-${version}.${ext}',
-  directories: {
-    output: 'build'
-  },
-  // default files: https://www.electron.build/configuration/contents
-  files: [
-    'package.json',
-    {
-      from: 'dist/main/',
-      to: 'dist/main/'
+    asar: true,
+    productName: "VKGram",
+    appId: "org.purplehorrorrus.vkgram",
+    artifactName: "vkgram-${version}.${ext}",
+    directories: {
+        output: "build"
     },
-    {
-      from: 'dist/renderer',
-      to: 'dist/renderer/'
-    },
-    {
-      from: 'src/resources/',
-      to: 'dist/resources/'
-    }
-  ],
-  ...windowsOS,
-  ...linuxOS,
-  ...macOS
-}
+    // default files: https://www.electron.build/configuration/contents
+    files: [
+        "package.json",
+        {
+            from: "dist/main/",
+            to: "dist/main/"
+        },
+        {
+            from: "dist/renderer",
+            to: "dist/renderer/"
+        },
+        {
+            from: "src/resources/",
+            to: "dist/resources/"
+        }
+    ],
+    ...windowsOS
+    // ...linuxOS,
+    // ...macOS
+};
